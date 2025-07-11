acaf05a7ee662b0a3bf0230f49a3ea7b559734fb
cpgomonit <christian.gomonit@gmail.com>
2025-07-11 09:49:08 +0800

    Replaced border-b with divide-y in archive-project.php and archive-article.php

--- archive-project.php
+++ archive-project.php
@@ -5,9 +5,10 @@
             <header>
                 <h1 class="text-white text-3xl md:text-4xl font-bold">my projects</h1>
             </header>
+            <div class="divide-y divide-[#6a809a]">
             <?php while (have_posts()) {
                 the_post(); ?>
-                <div class="post mt-20 border-b-1 border-[#6a809a] pb-20">
+                <div class="py-20">
                     <h2 class="text-2xl md:text-3xl font-bold"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                     <p class="mt-2 text-sm"><?php the_time('F jS Y'); ?></p>
                     <div class="mt-6 bg-white rounded-md max-w-3xl p-1 mx-auto">
@@ -16,9 +17,9 @@
                     <div class="text-sm md:text-base mt-6"><?php the_excerpt(); ?></div>
                     <a href="<?php the_permalink(); ?>" class="mt-6 inline-block bg-cyan-800 hover:bg-cyan-900 shadow-xl/20 rounded-full cursor-pointer px-6 py-2 transform hover:scale-105 hover:-translate-y-0.5 transition font-bold text-lg">Read more</a>
                 </div>
-            <?php } 
-
-            echo paginate_links(); ?>
+            <?php } ?>
+            </div>
+            <?php echo paginate_links(); ?>
         </div>
     </section>
 
